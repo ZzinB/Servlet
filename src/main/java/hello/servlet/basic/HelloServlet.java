2655100ba7deab47d9307232c57a3ef4683c04dd
ZzinB <mysteleee7@gmail.com>
2023-11-23 17:31:54 +0900

    Add: 추가 서블릿 코드

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet{
+    // 서블릿이 호출되면 service 메소드가 호출됨
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //메시지 header
+        response.setCharacterEncoding("utf-8");//메시지 header
+        response.getWriter().write("hello " + username); //메시지 body
+    }
+}
