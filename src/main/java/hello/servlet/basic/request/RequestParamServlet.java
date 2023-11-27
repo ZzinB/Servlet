d94770e1ebdc96c77990321864275924c4970ffe
ZzinB <mysteleee7@gmail.com>
2023-11-27 15:44:56 +0900

    Add: 추가 단일, 복수 파라미터 조회
    GET 쿼리 파라미터를 이용한 조회

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -12,6 +12,8 @@ import java.util.Enumeration;
 /*
     * 1. 파라미터 전송 기능
     * http://localhost:8080/request-param?username=hello&age=20
+    * 2. 동일한 파라미터 전송 가능
+    * http://localhost:8080/request-param?username=hello&username=lee&age=20
  */
 @WebServlet(name="requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
@@ -20,10 +22,29 @@ public class RequestParamServlet extends HttpServlet {
         System.out.println("[전체 파라미터 조회] - start");
        /* 예전방식
         Enumeration<String> parameterNames = request.getParameterNames();
-       */
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            System.out.println(paramName + "=" + request.getParameter(paramName));
+}       */
         request.getParameterNames().asIterator()
                         .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
 
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) { //단축키 iter
+            System.out.println("username = " + name); //단축키 soutv
+        }
+
+        response.getWriter().write("ok");
     }
 }
