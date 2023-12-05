e9ae0673852ea8ffd81fe3e30761ae8970e4a62d
ZzinB <mysteleee7@gmail.com>
2023-12-05 15:49:15 +0900

    Refac: 리팩토링 ControllerV1

--- src/main/java/hello/servlet/web/Frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/Frontcontroller/v1/ControllerV1.java
@@ -7,5 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface ControllerV1 {
-    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+    void process(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
 }
