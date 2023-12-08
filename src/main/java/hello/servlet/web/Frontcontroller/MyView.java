443df36e1140e068fe6091609b3a1f7ccc53a1c4
ZzinB <mysteleee7@gmail.com>
2023-12-08 22:47:44 +0900

    Add: 추가 새로운 render()

--- src/main/java/hello/servlet/web/Frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/Frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -17,7 +18,18 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+    // v3
+    // 모델 정보를 함께 받는 새로운 render()를 정의
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // key, value 변수명으로 루프 돌려서 model 원소 빼내기
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
 
-
+    }
+    // 모델의 데이터를 꺼내어 request에 담아둠
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
