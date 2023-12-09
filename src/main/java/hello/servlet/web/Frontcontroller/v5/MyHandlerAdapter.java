af02977e07c3733411beb51ff5a5910fa834bb3c
ZzinB <mysteleee7@gmail.com>
2023-12-09 16:30:49 +0900

    Code: 수정 오타수정

--- src/main/java/hello/servlet/web/Frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/Frontcontroller/v5/MyHandlerAdapter.java
@@ -1,13 +1,14 @@
 package hello.servlet.web.Frontcontroller.v5;
 
 import hello.servlet.web.Frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import javax.sql.rowset.serial.SerialException;
 import java.io.IOException;
 
-public interface MyHandlerAdaptor {
+public interface MyHandlerAdapter {
     boolean supprots(Object handler);
-    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws SerialException, IOException;
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
 }
