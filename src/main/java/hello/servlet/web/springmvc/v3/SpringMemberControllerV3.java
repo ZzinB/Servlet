6f65722bd16f4fea4f8a33c6910b698622bf20d1
ZzinB <mysteleee7@gmail.com>
2023-12-15 23:43:13 +0900

    Improve: 변경 매핑애노테이션

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -5,6 +5,8 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,12 +17,12 @@ import java.util.List;
 public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping
+    @GetMapping
     public String  members(Model model) {
         List<Member> members = memberRepository.findAll();
 
@@ -28,7 +30,7 @@ public class SpringMemberControllerV3 {
         return "members";
     }
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
